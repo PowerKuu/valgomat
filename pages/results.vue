38fe20f9153e845a03a3278331b94c47baacd4a0
PowerKuu <hkleven6@outlook.com>
2023-10-06 15:22:15 +0200

    update

--- pages/results.vue
+++ pages/results.vue
@@ -4,6 +4,10 @@ definePageMeta({
     layout: "valgomat"
 }) 
 
+function restart() {
+    sessionAnswers.value = null
+}
+
 const sessionAnswers = useSessionStorage("answers", null as string | null)
 
 const parsedAnswers = computed<{
@@ -11,13 +15,41 @@ const parsedAnswers = computed<{
 }>(() => JSON.parse(sessionAnswers.value ?? "{}"))
 
 
-const answers = computed(() => {
+const results = computed(() => {
+    const defaultResult = {
+        score: 0.5,
+        maxScore: 1
+    }
+
     const sumAnswers: {
-        [key: string]: {
+        [party: string]: {
             score: number
-            count: number
+            maxScore: number
         }
-    } = {}
+    } = Object.fromEntries(
+        parties.map((party) => ([party, defaultResult]))
+    )
+
+    for (const [questionId, answer] of Object.entries(parsedAnswers.value)) {
+        const question = config.questions.find(q => q.id === questionId)
+        if (!question) continue
+
+        for (const [party, partyAnswer] of Object.entries(question.ranks)) {
+            const existingAnswer = structuredClone(sumAnswers[party])
+    
+            existingAnswer.score += 1 - Math.abs(answer - partyAnswer)
+            existingAnswer.maxScore += 1
+
+            sumAnswers[party] = existingAnswer
+        }
+    }
+
+    return Object.entries(sumAnswers).map(([key, value]) => {
+        return {
+            party: key,
+            score: (value.score / value.maxScore)
+        }
+    }).sort((a, b) => b.score - a.score)
 })
 </script>
 
@@ -30,21 +62,40 @@ const answers = computed(() => {
 
         <Flex direction="column" gap="2rem" align="center">
 
-            {{ sessionAnswers }}
+            <Flex direction="column">
+                <Flex v-for="result of results">
+                    {{ result.party }}
 
-            <Flex gap="2rem" align="center">
-                <NuxtLink class="button" :href="getQuestionUrlByOrder(config.questions.length)">
+                    <p>{{ result.score }}</p>
+                </Flex>
+            </Flex>
+
+            <Flex gap="1rem">
+                <Flex gap="2rem" align="center">
+                    <NuxtLink class="button" :href="getQuestionUrlByOrder(config.questions.length)">
+                        <Button
+                            color="var(--contrast-color)" 
+                            gap="0.5rem" 
+                            icon="/icons/arrow-right.svg" 
+                            :reverse="true"
+                            :iconBefore="true"
+                        >
+                            Forrige
+                        </Button>
+                    </NuxtLink>
+                </Flex>
+
+                <NuxtLink @click="restart" href="/intro">
                     <Button
                         color="var(--contrast-color)" 
                         gap="0.5rem" 
                         icon="/icons/arrow-right.svg" 
                         :reverse="true"
-                        :iconBefore="true"
                     >
-                        Forrige
+                        Ta på nytt
                     </Button>
                 </NuxtLink>
-            </Flex> 
+            </Flex>
         </Flex>
     </Flex>
 </template>
--- utils/config.ts
+++ utils/config.ts
@@ -19,59 +19,89 @@ export interface Config {
     questions: Question[]
 }   
 
+
 const config: Config = {
     questions: [
         {
-            id: "name",
+            id: "klimaendringer",
             order: 1,
-
-            text: "Mener du at vi skal ha mindre skatt i Norge? 1",
+            text: "Mener du at det er nødvendig med øyeblikkelig handling for å bekjempe klimaendringer?",
             ranks: {
-                "ap": 0,
-                "frp": 0,
-                "høyre": 0,
-                "krf": 0,
-                "mdg": 0,
-                "rødt": 0,
-                "sp": 0,
-                "sv": 0,
-                "venstre": 0
+                "ap": 0.8,
+                "frp": 0.2,
+                "høyre": 0.6,
+                "krf": 0.5,
+                "mdg": 0.9,
+                "rødt": 0.9,
+                "sp": 0.7,
+                "sv": 0.9,
+                "venstre": 0.9
             }
         },
         {
-            id: "name1",
+            id: "helsevesen",
             order: 2,
-
-            text: "Mener du at vi skal ha mindre skatt i Norge? 2",
+            text: "Bør helsetjenester tilbys som en universell rettighet til alle borgere?",
             ranks: {
-                "ap": 0,
-                "frp": 0,
-                "høyre": 0,
-                "krf": 0,
-                "mdg": 0,
-                "rødt": 0,
-                "sp": 0,
-                "sv": 0,
-                "venstre": 0
+                "ap": 0.9,
+                "frp": 0.2,
+                "høyre": 0.4,
+                "krf": 0.6,
+                "mdg": 0.8,
+                "rødt": 0.9,
+                "sp": 0.8,
+                "sv": 0.9,
+                "venstre": 0.9
             }
         },
         {
-            id: "name2",
+            id: "utdanningsfinansiering",
             order: 3,
-
-            text: "Mener du at vi skal ha mindre skatt i Norge? 3",
+            text: "Støtter du økt finansiering av utdanning i nasjonalbudsjettet?",
             ranks: {
-                "ap": 0.5,
-                "frp": 0,
-                "høyre": 0,
-                "krf": 0,
-                "mdg": 0,
-                "rødt": 0,
-                "sp": 0,
-                "sv": 0,
-                "venstre": 0
+                "ap": 0.9,
+                "frp": 0.3,
+                "høyre": 0.6,
+                "krf": 0.7,
+                "mdg": 0.8,
+                "rødt": 0.9,
+                "sp": 0.9,
+                "sv": 0.9,
+                "venstre": 0.9
             }
-        } 
+        },
+        {
+            id: "innvandringspolitikk",
+            order: 4,
+            text: "Bør det innføres strengere innvandringspolitikk for å kontrollere grensesikkerhet?",
+            ranks: {
+                "ap": 0.4,
+                "frp": 0.9,
+                "høyre": 0.7,
+                "krf": 0.5,
+                "mdg": 0.2,
+                "rødt": 0.3,
+                "sp": 0.3,
+                "sv": 0.1,
+                "venstre": 0.2
+            }
+        },
+        {
+            id: "minstelønn",
+            order: 5,
+            text: "Støtter du økt minstelønn for arbeidstakere?",
+            ranks: {
+                "ap": 0.8,
+                "frp": 0.3,
+                "høyre": 0.5,
+                "krf": 0.6,
+                "mdg": 0.9,
+                "rødt": 0.9,
+                "sp": 0.9,
+                "sv": 0.9,
+                "venstre": 0.9
+            }
+        }
     ]
 }
 
